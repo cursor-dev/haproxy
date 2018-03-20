08a5ce614a2146eaa563ffdfa92ca5c6a49b82d4
Ryan Pederson <ryan@pederson.ca>
2018-03-20 11:15:22 -0600

    cleaned up start script

--- start.sh
+++ start.sh
@@ -1,29 +1,3 @@
 #!/bin/bash
 
-#
-# start.bash
-#
-
-HAPROXY="/config"
-OVERRIDE="/haproxy-override"
-PIDFILE="/var/run/haproxy.pid"
-
-CONFIG="haproxy.cfg"
-ERRORS="errors"
-
-cd "$HAPROXY"
-
-# Symlink errors directory
-if [[ -d "$OVERRIDE/$ERRORS" ]]; then
-  mkdir -p "$OVERRIDE/$ERRORS"
-  rm -fr "$ERRORS"
-  ln -s "$OVERRIDE/$ERRORS" "$ERRORS"
-fi
-
-# Symlink config file.
-if [[ -f "$OVERRIDE/$CONFIG" ]]; then
-  rm -f "$CONFIG"
-  ln -s "$OVERRIDE/$CONFIG" "$CONFIG"
-fi
-
-exec haproxy -f /config/haproxy.cfg -p "$PIDFILE"
+exec haproxy -f /config/haproxy.cfg -p /var/run/haproxy.pid
