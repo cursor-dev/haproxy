6aa4ac957a961ab2db1b3045ca40428bf135807d
Ryan Pederson <ryan@pederson.ca>
2018-03-20 08:47:02 -0600

    revamped start.sh file.

--- start.sh
+++ start.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-/usr/sbin/haproxy -c -f /config/haproxy.cfg
+
+#
+# start.bash
+#
+
+HAPROXY="/config"
+OVERRIDE="/haproxy-override"
+PIDFILE="/var/run/haproxy.pid"
+
+CONFIG="haproxy.cfg"
+ERRORS="errors"
+
+cd "$HAPROXY"
+
+# Symlink errors directory
+if [[ -d "$OVERRIDE/$ERRORS" ]]; then
+  mkdir -p "$OVERRIDE/$ERRORS"
+  rm -fr "$ERRORS"
+  ln -s "$OVERRIDE/$ERRORS" "$ERRORS"
+fi
+
+# Symlink config file.
+if [[ -f "$OVERRIDE/$CONFIG" ]]; then
+  rm -f "$CONFIG"
+  ln -s "$OVERRIDE/$CONFIG" "$CONFIG"
+fi
+
+exec haproxy -f /etc/haproxy/haproxy.cfg -p "$PIDFILE"
